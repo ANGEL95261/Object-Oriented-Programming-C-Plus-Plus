15e628ee6198b9f899d8f7f83c2c8484b6c5e301
ANGEL95261 <angelalexanderaguirre@gmail.com>
2024-02-04 18:19:05 -0800

    created lab1 sample

---
+++ Lab 1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * This lab is demonstrating that my IDE is properly set up 
+***/
+
+#include <iostream>
+#include <stdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0; 
+}
